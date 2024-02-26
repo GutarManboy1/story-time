efd278fb5fd8229fb1293a0cd0f8147ea2f6baed
Jason Sturgeon <zergyjanus@gmail.com>
2024-02-26 22:25:04 +0900

    asdf

---
+++ app/services/openai_service.rb
@@ -0,0 +1,23 @@
+require "openai"
+
+class OpenaiService
+  attr_reader :client, :prompt
+
+  def initialize(prompt)
+    @client = OpenAI::Client.new
+    @prompt = prompt
+  end
+
+  def call
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+        temperature: 0.7,
+        stream: false,
+        max_tokens: 100
+      }
+    )
+    return response["choices"][0]["message"]["content"]
+  end
+end
