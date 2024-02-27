de3e5be5f3defcd014ddebcdf11ba0fa15fcb9fd
alexander voeler <avoeler@gmail.com>
2024-02-27 11:21:33 +0900

    Fixed types in migration files

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,7 @@
 class Story < ApplicationRecord
   belongs_to :user
   belongs_to :prompt_template
+
+  validates :title, presence: true
+  validates :system_prompt, presence: true
 end
--- db/migrate/20240226093426_create_story_segments.rb
+++ db/migrate/20240226093426_create_story_segments.rb
@@ -1,10 +1,10 @@
 class CreateStorySegments < ActiveRecord::Migration[7.1]
   def change
     create_table :story_segments do |t|
-      t.integer :image
+      t.string :image
       t.integer :order
-      t.integer :message
-      t.integer :role
+      t.text :message
+      t.string :role
       t.references :story_id, null: false, foreign_key: true
 
       t.timestamps
